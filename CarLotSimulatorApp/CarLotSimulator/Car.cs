3836e3695bfa28f0f8a8800d95c130caacdb9a33
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-22 21:37:26 -0400

    Added the static CarLot class

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -6,10 +6,11 @@ namespace CarLotSimulator
 {
     public class Car
     {
-        //Create a seperate class file called Car
-        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-        //The methods should take one string parameter: the respective noise property
+        public Car()
+        {
+            CarLot.NumberOfCars++;
+            Console.WriteLine($"The Number of cars in the carlot now is: {CarLot.NumberOfCars}");
+        }
 
         public int Year { get; set; }
         public string Make { get; set; }
---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public static class CarLot
+    {
+        static public int NumberOfCars = 0;
+    }
+}
